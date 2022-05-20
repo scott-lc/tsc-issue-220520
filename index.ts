095deed9eef797a202a15d1e9b386eef59755ed8
Scott Sadler <scott@livecontrol.io>
2022-05-20 09:38:01 -0400

    .

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
 import clsx from "clsx";
 import classNames from "classnames";
 
+/*
+  index.ts:8:13 - error TS2349: This expression is not callable.
+  Type 'typeof import(".../tsc-issue-220520/node_modules/clsx/clsx")' has no call signatures.
+*/
 console.log(clsx("a", "b", "c"));
+
+/*
+  index.ts:14:13 - error TS2349: This expression is not callable.
+  Type 'typeof import(".../tsc-issue-220520/node_modules/classnames/index")' has no call signatures.
+*/
 console.log(classNames("a", "b", "c"));
